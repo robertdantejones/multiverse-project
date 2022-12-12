1385ed86705985df17e25c4eca0c1cca8e508319
Robert Jones <robertdante@outlook.com>
2022-12-12 14:21:41 -0500

    form validation

--- index.js
+++ index.js
@@ -2,3 +2,20 @@
 const task = new TaskManager(0);
 task.load();
 task.render();
+
+// store alerts to variables and set to hidden
+let valid = false;
+const nameAlert = document.getElementById('nameAlert');
+nameAlert.hidden = true;
+
+const describeAlert = document.getElementById('describeAlert');
+describeAlert.hidden = true;
+
+const categoryAlert = document.getElementById('categoryAlert');
+categoryAlert.hidden = true;
+
+const statusAlert = document.getElementById('statusAlert');
+statusAlert.hidden = true;
+
+const dateAlert = document.getElementById('dateAlert');
+dateAlert.hidden = true;
