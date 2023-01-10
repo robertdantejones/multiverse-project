d94deccdd50299e2fd0b237a8ba2303ed2ee52a1
Robert Jones <robertdante@outlook.com>
2023-01-10 00:19:05 -0500

    Update taskmanager.js

--- taskmanager.js
+++ taskmanager.js
@@ -39,5 +39,15 @@ class TaskManager {
          this.tasks.push(task);
       }
 
+      getTaskById (taskId) {
+        let foundTask; 
+        for (let i = 0; i < this.tasks.length; i++) {
+          const task = this.tasks[i];
+          if (task.id === taskId) {
+            foundTask = task;
+          }
+        }
+        return foundTask;
+      }
       
   };
